1bb903e0cabe3753e695be923ce1d4680530b041
Aldrin Jenson <aldrinjenson@gmail.com>
2022-02-03 00:59:22 +0530

    Refactoring

--- assembler/pass1.c
+++ assembler/pass1.c
@@ -1,76 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
 void main()
 {
-  FILE *f1, *f2, *f3, *f4;
-  int lc, sa, l, op1, o, len;
-  char m1[20], la[20], op[20], otp[20];
-  f1 = fopen("input.txt", "r");
-  f3 = fopen("symtab.txt", "w");
-  fscanf(f1, "%s%s%d", la, m1, &op1);
-  if (strcmp(m1, "START") == 0)
-  {
-    sa = op1;
-    lc = sa;
-    printf("\t%s\t%s\t%d\n", la, m1, op1);
-  }
-  else
-  {
-    lc = 0;
-  }
-  fscanf(f1, "%s%s", la, m1);
-  while (!feof(f1))
-  {
-    fscanf(f1, "%s", op);
-    printf("\n%d\t%s\t%s\n", lc, la, m1, op);
-    if (strcmp(la, "-") != 0)
-    {
-      fprintf(f3, "\n%d\t%s\n", lc, la);
-    }
-    f2 = fopen("optab.txt", "r");
-    fscanf(f2, "%s%d", otp, &o);
-    while (!feof(f2))
-    {
-      if (strcmp(m1, otp) == 0)
-      {
-        lc = lc + 3;
-        break;
-      }
-      fscanf(f2, "%s%d", otp, &o);
-    }
-    fclose(f2);
-    if (strcmp(m1, "WORD") == 0)
-    {
-      lc = lc + 3;
-    }
-    else if (strcmp(m1, "RESW") == 0)
-    {
-      op1 = atoi(op);
-      lc = lc + (3 * op1);
-    }
-    else if (strcmp(m1, "BYTE") == 0)
-    {
-      if (op[0] == 'X')
-      {
-        lc = lc + 1;
-      }
-      else
-      {
-        len = strlen(op) - 2;
-        lc = lc + len;
-      }
-    }
-    else if (strcmp(m1, "RESB") == 0)
-    {
-      op1 = atoi(op);
-      lc = lc + op1;
-    }
-    fscanf(f1, "%s%s", la, m1);
-  }
-  if (strcmp(m1, "END") == 0)
-  {
-    printf("program length=\n%d", lc - sa);
-  }
-  fclose(f1);
-  fclose(f3);
+	FILE *input = fopen("input.txt", "r");
+	FILE *opTable = fopen("optab.txt", "r");
+	FILE *output = fopen("out.txt", "w");
+	FILE *symTbl = fopen("symtbl.txt", "w");
+
+	char label[10], opcode[10], operand[10], code[10], mnemonic[10];
+
+	int lineCount = 0;
+	int locctr, startAddress = 0;
+	fscanf(input, "%s %s %s", label, opcode, operand);
+	if (strcmp(opcode, "START") == 0)
+	{
+		locctr = atoi(operand); // converting string to int
+		startAddress = atoi(operand);
+		fprintf(output, "      %s\t%s\t%s\n", label, opcode, operand);
+		fscanf(input, "%s %s %s", label, opcode, operand);
+	}
+	else
+		locctr = 0;
+	while (strcmp(opcode, "END") != 0)
+	{
+		fprintf(output, "%d\t", locctr);
+		if (strcmp(label, "**") != 0)
+		{
+			// add to symTable if it's defining a symbol
+			fprintf(symTbl, "%s\t%d\n", label, locctr);
+		}
+
+		fscanf(opTable, "%s %s", code, mnemonic);
+		while (strcmp(code, "END") != 0)
+		{
+			if (strcmp(code, opcode) == 0)
+			{
+				// like searching a hashmap, loop till te code matches the opcode from opTable
+				locctr += 3;
+				break;
+			}
+			fscanf(opTable, "%s %s", code, mnemonic);
+		}
+
+		if (strcmp(opcode, "WORD") == 0)
+			locctr += 3;
+		else if (strcmp(opcode, "RESW") == 0)
+			locctr += (3 * (atoi(operand)));
+		else if (strcmp(opcode, "RESB") == 0)
+			locctr += (atoi(operand));
+		else if (strcmp(opcode, "BYTE") == 0)
+			locctr++;
+
+		fprintf(output, "%s\t%s\t%s\n", label, opcode, operand);
+		fscanf(input, "%s\t%s\t%s", label, opcode, operand);
+	}
+	fprintf(output, "%d\t%s\t%s\t%s\n", locctr, label, opcode, operand);
+	int length = locctr - startAddress;
+	printf("Assembly code converted to intermediate file\n");
+	printf("The length of the code : %d\n", length);
+
+	fclose(input);
+	fclose(output);
+	fclose(opTable);
+	fclose(symTbl);
 }
